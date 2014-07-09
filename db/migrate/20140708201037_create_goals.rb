730fec0d80a9be37820205937150ba7528bd8344
kennytrionfo <kentrionfo@gmail.com>
2014-07-09 16:31:34 -0600

    added attributes to User_goal model and took out weekly_points_results from goals model

--- db/migrate/20140708201037_create_goals.rb
+++ db/migrate/20140708201037_create_goals.rb
@@ -6,7 +6,6 @@ class CreateGoals < ActiveRecord::Migration
       t.integer :point_value
       t.integer :frequency
       t.integer :weekly_points_goal
-      t.integer :weekly_points_results
       t.integer :user_id
 
       t.timestamps
--- db/migrate/20140708233138_create_user_goals.rb
+++ db/migrate/20140708233138_create_user_goals.rb
@@ -1,7 +1,12 @@
 class CreateUserGoals < ActiveRecord::Migration
   def change
     create_table :user_goals do |t|
-
+      t.text :category
+      t.integer :point_value
+      t.integer :frequency
+      t.integer :weekly_points_goal
+      t.integer :weekly_points_results
+      t.integer :user_id
       t.timestamps
     end
   end
