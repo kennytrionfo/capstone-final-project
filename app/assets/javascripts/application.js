4b375cef881f937bcff767d36b1beffbd2bf32a0
lcbunch <lisabunch6@gmail.com>
2014-07-19 14:01:35 -0600

    now using box grid for dashboard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,16 +30,18 @@ $(document).ready(function() {
 
   $('select').change(function() {
     var val1 = parseInt($(this).val());
-    var val2 = parseInt($(this).closest('tr').find('.point_value').text());
-    parseInt($(this).closest('tr').find('.weekly_goal').text(val1*val2));
+    console.log(val1);
+    var val2 = parseInt($(this).closest('li').find('.point_value').text());
+    console.log(val2);
+    parseInt($(this).closest('li').find('.weekly_goal').text(val1*val2));
   });
 
   // var weeklyResults = 0;
   $('a.done').click(function(){
-    var weekly_results = parseInt($(this).closest('tr').find('.weekly_results').text(), 10);
-    var point_value = parseInt($(this).closest('tr').find('.point_value').text(), 10);
+    var weekly_results = parseInt($(this).closest('li').find('.weekly_results').text(), 10);
+    var point_value = parseInt($(this).closest('li').find('.point_value').text(), 10);
     var total = weekly_results + point_value;
-    $(this).closest('tr').find('.weekly_results').text(total);
+    $(this).closest('li').find('.weekly_results').text(total);
     return false;
   });
       // parseInt($(this).closest('tr').find('.weekly_results').text(wr));
@@ -51,7 +53,7 @@ $(document).ready(function() {
 // // use an ajax call to save the change to the db.
   var theTotal = 0;
   $('a.done').click(function(){
-    theTotal = Number(theTotal) + parseInt($(this).closest('tr').find('.point_value').text());
+    theTotal = Number(theTotal) + parseInt($(this).closest('li').find('.point_value').text());
     $('.total').text("Your Grand Total: "+theTotal);
   });
       $('.total').text("Your Grand Total: "+theTotal);
