5636c18f0611b100cf569613e7f26121ccf8a34d
lcbunch <lisabunch6@gmail.com>
2014-07-23 16:16:25 -0600

    doing layout again

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,10 @@ ActiveRecord::Schema.define(version: 20140721222913) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.string   "username",                            null: false
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -41,6 +45,7 @@ ActiveRecord::Schema.define(version: 20140721222913) do
     t.integer  "grand_total",            default: 0
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
