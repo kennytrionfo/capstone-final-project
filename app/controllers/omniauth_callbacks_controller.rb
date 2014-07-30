d7f86e3a5e02ed6f6be5f18b56573b0733543232
lcbunch <lisabunch6@gmail.com>
2014-07-30 11:28:01 -0600

    added heroku fb keys

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       sign_in @user
-      redirect_to clients_goals_path, notice: 'User now signed in with facebook'
+      redirect_to root_path, notice: 'User now signed in with facebook'
     else
       redirect_to root_path, alert: 'Could not sign in'
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
       where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.username = auth.info.name 
+      user.username = auth.info.name
     end
   end
 
@@ -21,14 +21,6 @@ class User < ActiveRecord::Base
   enum role: [:client, :admin]
   validates :username, :email, :encrypted_password, presence: true
 
-  # def self.from_omniauth(auth)
-  # where(auth.slice(:provider, :uid)).first_or_create do |user|
-  #   user.email = auth.info.email
-  #   user.password = Devise.friendly_token[0,20]
-  #   user.name = auth.info.name   # assuming the user model has a name
-  #   user.image = auth.info.image # assuming the user model has an image
-  #   end
-  # end
   #
   # def self.new_with_session(params, session)
   #   super.tap do |user|
