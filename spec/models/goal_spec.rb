6a455eadcd008bad4670c00b25f04c856f82b1d1
lcbunch <lisabunch6@gmail.com>
2014-07-29 18:09:45 -0600

    model testing done

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -2,5 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Goal, :type => :model do
 
+  describe 'percentage' do
+    before do
+      @goal = Goal.create(weekly_points_goal: 100, weekly_results: 50)
+    end
+    it 'calculates the percentage' do
+      expect(@goal.percentage).to eq 50
+    end
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,41 +2,40 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
 
-  describe 'associations' do
-    it {should have_many :user_goals}
-  end
-
-  describe 'validations' do
-    it {should validate_presence_of :username}
-    it {should validate_presence_of :email}
-    it {should validate_presence_of :encrypted_password}
-  end
-
-  describe '#username' do
-    before do
-      @user = create(:user, username: 'marylouwho', email: 'this@rspec.com', password: 'password')
+    describe 'associations' do
+      it {should have_many :goals}
     end
-    it 'returns marylouwho' do
-      expect(@user.username).to eq 'marylouwho'
-    end
-  end
 
-  describe '#email' do
-    before do
-      @user = create(:user, username: 'marylouwho', email: 'this@rspec.com', password: 'password')
+    describe 'validations' do
+      it {should validate_presence_of :username}
+      it {should validate_presence_of :email}
+      it {should validate_presence_of :encrypted_password}
     end
-    it 'returns this@rspec.com' do
-      expect(@user.email).to eq 'this@rspec.com'
-    end
-  end
 
-  describe '#password' do
-    before do
-      @user = create(:user, username: 'marylouwho', email: 'this@rspec.com', password: 'password')
+    describe '#username' do
+      before do
+        @user = create(:user, username: 'marylouwho', email: 'this@rspec.com', password: 'password')
       end
-    it 'returns this@rspec.com' do
-      expect(@user.password).to eq 'password'
+      it 'returns marylouwho' do
+        expect(@user.username).to eq 'marylouwho'
+      end
     end
-  end
 
+    describe '#email' do
+      before do
+        @user = create(:user, username: 'marylouwho', email: 'this@rspec.com', password: 'password')
+      end
+      it 'returns this@rspec.com' do
+        expect(@user.email).to eq 'this@rspec.com'
+      end
+    end
+
+    describe '#password' do
+      before do
+        @user = create(:user, username: 'marylouwho', email: 'this@rspec.com', password: 'password')
+      end
+      it 'returns this@rspec.com' do
+        expect(@user.password).to eq 'password'
+      end
+    end
 end
